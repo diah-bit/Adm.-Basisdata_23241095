cad82f81fe11dc152b1541d67de267946529380a
diah-bit <148064699+diah-bit@users.noreply.github.com>
2025-04-16 18:57:23 +0800

    Add files via upload

---
+++ Pre-Praktikum/Pre-2.sql
@@ -0,0 +1,19 @@
+-- Nama : Halimatus sa'diah
+-- NIM : 23241095
+-- Kelas : C 
+
+-- Melihat database yang ada
+SHOW DATABASES;
+
+-- Memilih databases
+USE mysql;
+
+-- Melihat tabel dalam mysql
+SHOW TABLES;
+
+-- Mendeskripsikan sebuah tabel yang dipilih ex, tabel, 'user'
+DESCRIBE user; 
+
+-- Melihat isi tabel user, khusus untuk kolom Host, user, dan password
+SELECT host, user, password FROM user;
+
